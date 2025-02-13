3d38211003c8c199363d94eb252940c4a604d8e1
yarin1849 <yarin1849@gmail.com>
2025-02-13 13:40:06 +0200

    Improve details page style

--- src/pages/StayDetails.jsx
+++ src/pages/StayDetails.jsx
@@ -3,6 +3,9 @@ import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
+import share from '../assets/details-action-icons/share.svg'
+import save from '../assets/details-action-icons/save.svg'
+
 import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service'
 import { loadStay, addStayMsg } from '../store/actions/stay.actions'
 import { StayGallery } from '../cmps/StayGallery'
@@ -31,9 +34,14 @@ export function StayDetails() {
   }
 
   return (
-    <section className="stay-details">
-      {/* <Link to="/stay">Back to list</Link> */}
-      <h1>Sunset Luxury 6 Bedroom Villa with Swimming pool</h1>
+    <section className="stay-details full">
+      <div className='details-header'>
+        <h1>Sunset Luxury 6 Bedroom Villa with Swimming pool</h1>
+        <div className='action-btns'>
+          <button className='action-btn' ><img src={share} alt="Main" className='action-img' /> Share</button>
+          <button className='action-btn'><img src={save} alt="Main" className='action-img' /> Save</button>
+        </div>
+      </div>
       {stay && <div>
         <StayGallery />
         <StayDescription />
@@ -42,7 +50,6 @@ export function StayDetails() {
       </div>
       }
       {/* <button onClick={() => { onAddStayMsg(stay._id) }}>Add stay msg</button> */}
-
     </section>
   )
 }
