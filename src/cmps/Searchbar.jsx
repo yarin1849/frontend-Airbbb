009c8518e88ab6b9b39e8cc57fafc28513fb553c
hoshen katz <hoshen.katz@gmail.com>
2025-02-14 15:25:35 +0200

    Add seperators with pseudo element

--- src/cmps/Searchbar.jsx
+++ src/cmps/Searchbar.jsx
@@ -12,21 +12,21 @@ export function SearchBar() {
                         <div>Where</div>
                         <input type="text" id="Where" placeholder="Search destination" />
                     </label>
-                <div className="seperator"></div>
+                
                 </div>
                 <div className='input-container flex'>
                     <label htmlFor="">
                         <div>Check in</div>
                         <input type="text" id="" placeholder="Add dates" />
                     </label>
-                <div className="seperator"></div>
+                
                 </div>
                 <div className='input-container flex'>
                     <label htmlFor="">
                         <div>Check out</div>
                         <input type="text" id="Where" placeholder="Add dates" />
                     </label>
-                <div className="seperator"></div>
+                
                 </div>
                 <div  className='input-container flex'>
                     <label htmlFor="">
