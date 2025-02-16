3c37a5efff3e0846d3c4c63e60d59caef8a67bef
yarin1849 <yarin1849@gmail.com>
2025-02-16 12:00:40 +0200

    Add more style to Booking page

--- src/cmps/AppFooter.jsx
+++ src/cmps/AppFooter.jsx
@@ -5,12 +5,12 @@ export function AppFooter() {
 
 	return (
 		<footer className="app-footer full">
-			<p>Coffeerights &copy; 2024</p>
+			<p>&copy; 2025 Airbnb, Inc.</p>
 			<p>Count: {count}</p>
-            
-            {import.meta.env.VITE_LOCAL ? 
-                <span className="local-services">Local Services</span> : 
-                <span className="remote-services">Remote Services</span>}
+
+			{import.meta.env.VITE_LOCAL ?
+				<span className="local-services">Local Services</span> :
+				<span className="remote-services">Remote Services</span>}
 		</footer>
 	)
 }
--- src/pages/StayBooking.jsx
+++ src/pages/StayBooking.jsx
@@ -12,6 +12,14 @@ const images = [
     {
         name: "google-icon",
         src: "https://res.cloudinary.com/du312ufuo/image/upload/v1739635520/google-icon_gumgnk.svg"
+    },
+    {
+        name: "verification-icon",
+        src: "https://res.cloudinary.com/du312ufuo/image/upload/v1739695908/svg_xml_base64_PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzMiAzMiIgYXJpYS1oaWRkZW49InRydWUiIHJvbGU9InByZXNlbnRhdGlvbiIgZm9jdXNhYmxlPSJmYWxzZSIgc3R5bGU9ImRpc3BsYXk6IGJsb2NrOyBoZWlnaH_4_plv6ui.svg"
+    },
+    {
+        name: "diamond-icon",
+        src: "https://res.cloudinary.com/du312ufuo/image/upload/v1739697775/diamond_iuzl5x.svg"
     }
 ]
 
@@ -27,21 +35,38 @@ export function StayBooking() {
                     <h1>Confirm and Pay</h1>
                 </div>
             </div>
-            <h2>Your trip</h2>
-            <div className="booking-dates">
-                <h3>Dates</h3>
-                <span>Mar 1-6</span>
+            <div className="booking-user-details-connected">
+                {/* <div className="booking-verification">
+                    <h2>Hi, you’re logged in</h2>
+                    <img src={images[3]?.src}></img>
+                </div> */}
+                <div className="recommendation-container">
+                    <div >
+                        <h2>This is a rare find.</h2>
+                        <span>Bros Lee's place is usually booked.</span>
+                    </div>
+                    <div>
+                        <img src={images[4]?.src} />
+                    </div>
+                </div>
             </div>
-            <div className="booking-guests">
-                <h3>Dates</h3>
-                <span>Mar 1-6</span>
+            <div className="trip-details">
+                <h2>Your trip</h2>
+                <div className="booking-dates">
+                    <h3>Dates</h3>
+                    <span>Mar 1-6</span>
+                </div>
+                <div className="booking-guests">
+                    <h3>Guests</h3>
+                    <span>2</span>
+                </div>
+                <hr></hr>
             </div>
             <div className="booking-user-details-not-connected">
                 <h3>Welcome back, Yarin</h3>
                 <img src={images[1]?.src} />
                 <span>ab*********98@gmail.com</span>
             </div>
-
-        </div >
+        </div>
     )
 }
