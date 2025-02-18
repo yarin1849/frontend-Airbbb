1e36e12b5be6973bf1fc3764580f86ff443fa269
hoshen katz <hoshen.katz@gmail.com>
2025-02-18 10:55:58 +0200

    Refactor details to full data model

--- src/cmps/StayGallery.jsx
+++ src/cmps/StayGallery.jsx
@@ -1,23 +1,15 @@
 import { useEffect, useState } from 'react';
 import { stayService } from '../services/stay/stay.service.local';
 
-export function StayGallery() {
-    const [imgUrls, setImgUrls] = useState([]);
-
-    useEffect(() => {
-        async function loadStayImages() {
-            const stays = await stayService.query()
-            if (stays.length > 0) setImgUrls(stays[0].imgUrls)
-        }
-        loadStayImages()
-    }, []);
-
+export function StayGallery({stay}) {
+   
+   
     return (
         <div className="stay-gallery">
-            {imgUrls.length > 0 ? (
+            {stay.imgUrls.length > 0 ? (
                 <>
-                    <img src={imgUrls[0]} alt="Main" className="stay-img" />
-                    {imgUrls.slice(1).map((imgUrl, index) => (
+                    <img src={stay.imgUrls[0]} alt="Main" className="stay-img" />
+                    {stay.imgUrls.slice(1).map((imgUrl, index) => (
                         <img key={index} src={imgUrl} alt={`Gallery ${index + 1}`} />
                     ))}
                 </>
