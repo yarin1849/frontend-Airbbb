b6492e8017b5f0887b14491baca5a32800ee0e5d
hoshen katz <hoshen.katz@gmail.com>
2025-02-19 11:48:52 +0200

    Link where and date modals to filter

--- src/cmps/WhereModal.jsx
+++ src/cmps/WhereModal.jsx
@@ -1,31 +1,34 @@
-import { useEffect } from "react"
+import { useState } from "react"
 
 
-export function WhereModal({setFilterByToEdit, FilterByToEdit}) {
-// useEffect(()=>{
-//     handleClick()
-// },[FilterByToEdit])
 
-function handleClick({place} = ''){
-    console.log(place)
-    setFilterByToEdit(prevFilter => ({ ...prevFilter, where: place }))
-}
+export function WhereModal({ setFilterByToEdit, FilterByToEdit }) {
+
+    const [place, setPlace] = useState('')
+
+    function handleClick( place ) {
+        console.log(place)
+        // setPlace(place)
+        setFilterByToEdit(prevFilter => ({ ...prevFilter, where: place }))
+    }
+
+
 
     return (
         <div className="where-modal">
-            <article onClick={handleClick(()=>{'united States'})}>
+            <article onClick={() => { handleClick('united States') }}>
                 United States <br />
                 <span>State of mind</span>
             </article>
-            <article onClick={handleClick(()=>{'spain'})}>
+            <article onClick={() => { handleClick('Spain') }}>
                 Spain <br />
                 <span>Sea side tapas bar</span>
             </article>
-            <article onClick={handleClick(()=>{'ortugal'})}>
+            <article onClick={() => { handleClick('portugal') }}>
                 Portugal <br />
                 <span>Glass of wine in porto</span>
             </article>
-            <article onClick={handleClick(()=>{'brazil'})}>
+            <article onClick={() => { handleClick('Brazil') }}>
                 Brazil <br />
                 <span>Tropical heaven vacation</span>
             </article>
